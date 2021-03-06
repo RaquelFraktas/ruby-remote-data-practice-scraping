fe729737e6ca229f94ecedd534948b3df421fe4a
github username <github email address>
2021-03-06 13:48:52 -0500

    commit tree 8330eb624250bc7c84429e28e1347ebef906c6f8

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/")) 
+puts doc.css(".site-header__hero__headline") 
